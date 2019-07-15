783bd856b017dad6d5f81e3d89108a1fd648650e
swat1604 <sraturi757@gmail.com>
2019-07-16 01:37:30 +0530

    update code

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
         selector: 'app-warning-alert',
-        template:'<p>This is warning, you are in danger</p>',
+        template:'<p>This is warning, you are in danger!</p>',
         styles:[
             `p{
                 background-color: mistyrose;
