b5da786f78f6cd2ffe5565046b60fa8f7db8870a
swat1604 <sraturi757@gmail.com>
2019-07-16 22:09:08 +0530

    code upsate

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,11 @@ import { Component } from '@angular/core';
     templateUrl :'./server.component.html'
 })
 export class ServerComponent{
+    serverId: number =10;
+    serverStatus :string ='Offline';
+
+    getServerStatus(){
+        return this.serverStatus;
+    }
 
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
- // selector: 'app-servers',
- selector: '.app-servers',
- templateUrl: './servers.component.html', // for file url
-  //template: '<app-server></app-server><app-server></app-server>', // for inline code
+ selector: 'app-servers',
+ //selector: '[app-servers]', for <div app-server></div>
+ //selector: '.app-servers', // for <div class='app-server'></div>
+ //template: '<app-server></app-server><app-server></app-server>', // for inline code
+  templateUrl: './servers.component.html', // for file url
   styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  
+  constructor() {
+    setTimeout(()=> {
+      this.allowNewServer = true;
+    }, 2000);
+    }
 
   ngOnInit() {
   }
