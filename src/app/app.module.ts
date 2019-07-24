fac956efb366f182045ecea9e066287b6f92d85c
swat1604 <sraturi757@gmail.com>
2019-07-24 22:31:02 +0530

    code update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -19,6 +20,7 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus="No server was created!";
-  serverName="";
+  serverName="TestServer";
   
   constructor() {
     setTimeout(()=> {
@@ -22,7 +22,7 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
   }
   onCreateServer(){
-    this.serverCreationStatus = "Server was created!";
+    this.serverCreationStatus = 'Server was created! Name is'+ this.serverName;
   }
 
   onUpdateServerName(event: Event){
